4a33da8856ef5bdb08d590ee315caf372c351e55
Gaga_riiiiiiin <80557945+yuritaka10@users.noreply.github.com>
2023-03-07 09:59:46 +0900

    1st commit

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = 'gyro_horse';
+var urlsToCache = [
+    '/gyro_horse/js/main.js',
+    '/gyro_horse/css/style.css'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
